22ab39d92550b5197d491f6653c81f183a673dcb
Vincent <1061271330@qq.com>
2019-06-26 18:07:16 +0800

    first commit

---
+++ src/test/testStudent.java
@@ -0,0 +1,10 @@
+package test;
+
+import model.Student;
+
+public class testStudent {
+    public static void main(String[] args) {
+        Student student = new Student("Vincent",23,"男","123456789");
+        System.out.println(student);
+    }
+}
